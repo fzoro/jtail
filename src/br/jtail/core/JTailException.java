427f20fe5aa3554d21a4bd32a4f2da9481cdf1a7
fabiozoroastro <fabiozoroastro@gmail.com>
2013-10-10 21:48:35 -0300

    JTailException to jdk6

--- src/br/jtail/core/JTailException.java
+++ src/br/jtail/core/JTailException.java
@@ -1,39 +1,28 @@
 package br.jtail.core;
 
-public class JTailException extends Exception
-{
+public class JTailException extends Exception {
 
 	/**
 	 *
 	 */
 	private static final long serialVersionUID = 1L;
 
-	public JTailException()
-	{
+	public JTailException() {
 		// TODO Auto-generated constructor stub
 	}
 
-	public JTailException(String message)
-	{
+	public JTailException(String message) {
 		super(message);
 		// TODO Auto-generated constructor stub
 	}
 
-	public JTailException(Throwable cause)
-	{
+	public JTailException(Throwable cause) {
 		super(cause);
 		// TODO Auto-generated constructor stub
 	}
 
-	public JTailException(String message, Throwable cause)
-	{
+	public JTailException(String message, Throwable cause) {
 		super(message, cause);
-		// TODO Auto-generated constructor stub
-	}
-
-	public JTailException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace)
-	{
-		super(message, cause, enableSuppression, writableStackTrace);
 		// TODO Auto-generated constructor stub
 	}
 
